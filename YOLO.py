fb8aab9cc757de78aef27c48bfc898fc31206b93
dotdak <honghung.dotdak@gmail.com>
2019-12-15 04:34:49 +0700

    Add files via upload

--- YOLO.py
+++ YOLO.py
@@ -4,13 +4,14 @@
 # Website : http://www.arunponnusamy.com
 ############################################
 
+import os
 import time
 import cv2
 import argparse
 import numpy as np
 
 ap = argparse.ArgumentParser()
-ap.add_argument('-i', '--image', required=True,
+ap.add_argument('-i', '--input', required=True,
                 help='path to input image')
 ap.add_argument('-c', '--config', required=True,
                 help='path to yolo config file')
@@ -40,68 +41,77 @@ def draw_prediction(img, class_id, confidence, x, y, x_plus_w, y_plus_h):
 
     cv2.putText(img, label, (x - 10, y - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.5, color, 2)
 
+list_file = [f for f in os.listdir(args.input) if os.path.isfile(os.path.join(args.input, f))]
+for file in list_file:
+    name = file[:-4] + '.txt'
+    image = cv2.imread(os.path.join(args.input, file))
 
-image = cv2.imread(args.image)
+    Width = image.shape[1]
+    Height = image.shape[0]
+    scale = 0.00392
 
-Width = image.shape[1]
-Height = image.shape[0]
-scale = 0.00392
+    classes = None
 
-classes = None
+    with open(args.classes, 'r') as f:
+        classes = [line.strip() for line in f.readlines()]
 
-with open(args.classes, 'r') as f:
-    classes = [line.strip() for line in f.readlines()]
+    COLORS = np.random.uniform(0, 255, size=(len(classes), 3))
 
-COLORS = np.random.uniform(0, 255, size=(len(classes), 3))
+    net = cv2.dnn.readNet(args.weights, args.config)
 
-net = cv2.dnn.readNet(args.weights, args.config)
+    blob = cv2.dnn.blobFromImage(image, scale, (416, 416), (0, 0, 0), True, crop=False)
 
-blob = cv2.dnn.blobFromImage(image, scale, (416, 416), (0, 0, 0), True, crop=False)
+    net.setInput(blob)
 
-net.setInput(blob)
+    outs = net.forward(get_output_layers(net))
 
-outs = net.forward(get_output_layers(net))
+    class_ids = []
+    confidences = []
+    boxes = []
+    conf_threshold = 0.5
+    nms_threshold = 0.4
 
-class_ids = []
-confidences = []
-boxes = []
-conf_threshold = 0.5
-nms_threshold = 0.4
+    # Thực hiện xác định bằng HOG và SVM
+    # start = time.time()
 
-# Thực hiện xác định bằng HOG và SVM
-start = time.time()
+    for out in outs:
+        for detection in out:
+            scores = detection[5:]
+            class_id = np.argmax(scores)
+            confidence = scores[class_id]
+            if confidence > 0.5:
+                center_x = int(detection[0] * Width)
+                center_y = int(detection[1] * Height)
+                w = int(detection[2] * Width)
+                h = int(detection[3] * Height)
+                x = center_x - w / 2
+                y = center_y - h / 2
+                class_ids.append(class_id)
+                confidences.append(float(confidence))
+                boxes.append([x, y, w, h])
 
-for out in outs:
-    for detection in out:
-        scores = detection[5:]
-        class_id = np.argmax(scores)
-        confidence = scores[class_id]
-        if confidence > 0.5:
-            center_x = int(detection[0] * Width)
-            center_y = int(detection[1] * Height)
-            w = int(detection[2] * Width)
-            h = int(detection[3] * Height)
-            x = center_x - w / 2
-            y = center_y - h / 2
-            class_ids.append(class_id)
-            confidences.append(float(confidence))
-            boxes.append([x, y, w, h])
+    indices = cv2.dnn.NMSBoxes(boxes, confidences, conf_threshold, nms_threshold)
 
-indices = cv2.dnn.NMSBoxes(boxes, confidences, conf_threshold, nms_threshold)
+    prediction = ''
 
-for i in indices:
-    i = i[0]
-    box = boxes[i]
-    x = box[0]
-    y = box[1]
-    w = box[2]
-    h = box[3]
-    draw_prediction(image, class_ids[i], confidences[i], round(x), round(y), round(x + w), round(y + h))
+    for i in indices:
+        i = i[0]
+        box = boxes[i]
+        x = box[0]
+        y = box[1]
+        w = box[2]
+        h = box[3]
+        score = confidences[i]
+        prediction += '{},{},{},{},{}\n'.format(x, y, w, h, score)
 
-cv2.imwrite(args.output + ".jpg", image)
+    with open(os.path.join(args.output, name), 'w') as f:
+        f.write(prediction)
+    # draw_prediction(image, class_ids[i], confidences[i], round(x), round(y), round(x + w), round(y + h))
 
-end = time.time()
-print("YOLO Execution time: " + str(end-start))
+# cv2.imwrite(args.output, image)
+
+# end = time.time()
+# print("YOLO Execution time: " + str(end-start))
 
 
 # cv2.waitKey()
